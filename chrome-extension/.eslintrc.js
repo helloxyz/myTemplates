8a2deb41f9f0c2ed4025d016d48b72fd52523022
helloxyz <helloxyz425@hotmail.com>
2018-10-03 16:00:50 +0800

    introduce chrome extension template

---
+++ chrome-extension/.eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true,
+        "jquery": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 2018
+    },
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "windows"
+        ],
+        "semi": [
+            "error",
+            "never"
+        ],
+        "no-console": "off",
+    }
+};
---
+++ chrome-extension/README.md
@@ -0,0 +1,26 @@
+# Chrome Extension Template
+> template for dev
+
+Overview
+---
+- jQuery v3.1.1
+- bootstrap v4.0.0-alpha.3
+
+Structure
+---
+```
+.
+│── index.js
+│── index.css
+│── package.json
+│── manifest.json
+│── icon.ico
+└── lib 
+    │── bootstrap.min.css
+    │── jquery.min.js
+    └── websocket.js
+
+```
+License
+---
+[MIT](LICENSE)
